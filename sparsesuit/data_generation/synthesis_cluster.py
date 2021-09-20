d4a24ced274ad8121703fc7a4df74c9bd279cefb
nkueng <nkueng@ethz.ch>
2021-09-20 15:30:42 +0200

    adding synthesis for cluster

---
+++ sparsesuit/data_generation/synthesis_cluster.py
@@ -0,0 +1,26 @@
+""" A script to handle the synthesis of IMU data from 17 sensors based on the AMASS dataset of SMPL pose data. """
+import os
+import sys
+
+import hydra
+import submitit
+from omegaconf import DictConfig
+
+from sparsesuit.data_generation.normalization import Normalizer
+from sparsesuit.data_generation.synthesis import Synthesizer
+
+
+@hydra.main(config_path="conf", config_name="synthesis_cluster")
+def do_synthesis(cfg: DictConfig):
+    # reads hydra config to run on SLURM cluster
+    env = submitit.JobEnvironment()
+
+    syn = Synthesizer(cfg=cfg)
+    syn.synthesize_dataset()
+
+    norm = Normalizer(cfg=cfg)
+    norm.normalize_dataset()
+
+
+if __name__ == "__main__":
+    do_synthesis()
