8cb3d6762157fa89457e637bc7ac1f51bbb68892
nkueng <nkueng@ethz.ch>
2021-10-29 15:39:38 +0200

    fix finetuning

--- sparsesuit/learning/finetuning.py
+++ sparsesuit/learning/finetuning.py
@@ -1,4 +1,7 @@
 import hydra
+import submitit
+
+from sparsesuit.constants import paths
 from sparsesuit.learning.training import Trainer
 from sparsesuit.learning.evaluation import Evaluator
 import os
@@ -6,9 +9,14 @@ import sys
 from sparsesuit.utils import utils
 from omegaconf import DictConfig, OmegaConf
 
+finetune_config_name = "finetuning_cluster" if paths.ON_CLUSTER else "finetuning"
 
-@hydra.main(config_path="conf", config_name="finetuning")
+
+@hydra.main(config_path="conf", config_name=finetune_config_name)
 def do_finetuning(cfg: DictConfig):
+    if paths.ON_CLUSTER:
+        submitit.JobEnvironment()
+
     try:
         trainer = Trainer(cfg=cfg, finetune=True)
         trainer.train()
@@ -24,12 +32,12 @@ def do_finetuning(cfg: DictConfig):
     eval_cfg = OmegaConf.load(eval_cfg_path)
     # adapt evaluation dataset to finetuning dataset
     eval_cfg.evaluation.experiment = trainer.experiment_name
-    eval_cfg.evaluation.eval_dataset = cfg.experiment.train_dataset
+    eval_cfg.evaluation.eval_dataset = cfg.finetune_dataset
     # keep debugging and noise flag but disable visualization
     eval_cfg.debug = cfg.debug
     eval_cfg.visualize = False
-    eval = Evaluator(cfg=eval_cfg)
-    eval.evaluate()
+    evaluator = Evaluator(cfg=eval_cfg)
+    evaluator.evaluate()
 
 
 if __name__ == "__main__":
